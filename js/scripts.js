bd02a9e540c493e987cab5e6450ea19fa2546ace
Dawson Mortenson and Noah Gottlieb <dawson@>
2017-08-17 11:58:14 -0700

    create jquery and tests

--- js/scripts.js
+++ js/scripts.js
@@ -1,53 +1,50 @@
+var romTrans = function(userNum){
+
 var theRomans=[];
-var thous=["M"];
-var hunds=["C","D","M"];
-var tens=["X","L","C"];
-var ones=["I","V","X"];
+var arrayNums=[];
 
-var arrayOfRNs = [thous, hunds, tens, ones];
+var romanNums = [["M"],["C","D","M"],["X","L","C"],["I","V","X"]];
 
-var nputNum = prompt("Enter a number to convert to Roman Numerals");
 
-var ms = Math.floor(inputNum/1000);
-inputNum = inputNum % 1000;
-var cs = Math.floor(inputNum/100);
-inputNum = inputNum % 100;
-var xs = Math.floor(inputNum/10);
-inputNum = inputNum % 10;
-var is = Math.floor(inputNum/1);
+for(var i = 1000; i >= 1; i = i/10){
+  arrayNums.push(Math.floor(userNum/i));
+  userNum = userNum % i;
+}
 
-arrayNums = [ms, cs, xs, is];
+  for(var i = 0; i < 4; i++){
+  		if(arrayNums[i] <= 3){
+        for(var j = 0; j < arrayNums[i]; j++){
+          theRomans.push(romanNums[i][0]);
+         };
+      } else if (arrayNums[i] === 4){
+        theRomans.push(romanNums[i][0]);
+        theRomans.push(romanNums[i][1]);
+      } else if (arrayNums[i] === 5){
+        theRomans.push(romanNums[i][1]);
+      } else if (arrayNums[i] <= 8){
+        theRomans.push(romanNums[i][1]);
+        for(var j = 0; j < (arrayNums[i] % 5); j++){
+          theRomans.push(romanNums[i][0]);
+        };
+      } else if (arrayNums[i] === 9){
+        theRomans.push(romanNums[i][0]);
+        theRomans.push(romanNums[i][2]);
+      }
 
-for(var i = 0; i < 4; i++){
-		if(arrayNums[i] <= 3){
-      for(var j = 0; j < arrayNums[i]; j++){
-        theRomans.push(arrayOfRNs[i][0]);
-       };
-    } else if (arrayNums[i] === 4){
-      theRomans.push(arrayOfRNs[i][0]);
-      theRomans.push(arrayOfRNs[i][1]);
-    } else if (arrayNums[i] === 5){
-      theRomans.push(arrayOfRNs[i][1]);
-    } else if (arrayNums[i] <= 8){
-      theRomans.push(arrayOfRNs[i][1]);
-      for(var j = 0; j < (arrayNums[i] % 5); j++){
-        theRomans.push(arrayOfRNs[i][0]);
-      };
-    } else if (arrayNums[i] === 9){
-      theRomans.push(arrayOfRNs[i][0]);
-      theRomans.push(arrayOfRNs[i][2]);
-    }
-  console.log(theRomans.join(""));
+
+  };
+    return theRomans;
 };
 
 
 
 
-
 $(document).ready(function(){
-  ("#romanNum").submit(function(event){
+  $("form#romanNum").submit(function(event){
     event.preventDefault();
-
+      var userNumber = parseInt($("#numInput").val());
+      var result = romTrans(userNumber);
+    $("#output").show().text(result.join(""));
 
   });
 });
